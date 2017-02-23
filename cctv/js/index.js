ed9b62b89f7cffef22269f68d1352309f677642e
szybj <songzhiyin@outlook.com>
2017-02-23 14:11:27 +0800

    end

--- cctv/js/index.js
+++ cctv/js/index.js
@@ -2,8 +2,8 @@
 var imgData = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg'];
 window.onload = function() {
     creat();
-    tabFn();
     scoreFn();
+formIn()
 }
 
 function creat() {
@@ -14,9 +14,9 @@ function creat() {
     }
     picList.innerHTML = str;
 }
-bind(document, 'touchmove', function(ev) {
+/*bind(document, 'touchmove', function(ev) {
     ev.preventDefault();
-})
+})*/
 function tabFn() {
     var oTab = id('tabPic');
     var oList = id('picList');
@@ -76,14 +76,14 @@ function tabFn() {
          addClass(aNav[iNow], 'active');
     }
 }
-function load() {
+function loadFn() {
     var iTime = new Date().getTime();
     var oW = id('welcome');
     var arr = [];
     var bImgLoad = false;
     var bTime = false;
     var oTimer = null;
-    bind(oW, 'webKitTransitionEnd', end);
+    bind(oW, 'WebkitTransitionEnd', end);
     bind(oW, 'transitionend',  end);
     oTimer = setInterval(function() {
         if (new Date().getTime() - iTime >=5000) {
@@ -98,6 +98,7 @@ function load() {
     },1000);
     function end() {
         removeClass(oW, 'pageShow');
+         tabFn();
     }
 };
 
@@ -125,4 +126,131 @@ function scoreFn() {
             })
         }
     }
+    indexFn();
+}
+function infoFn(obj, text) {
+    obj.innerHTML = text;
+    obj.style.WebkitTransform = 'scale(1)';
+    obj.style.transform = 'scale(1)';
+    obj.style.opacity = 1;
+    setTimeout(function() {
+        obj.style.WebkitTransform = 'scale(0)';
+        obj.style.tTransform = 'scale(0)';
+        obj.style.opacity = 0;
+    },1000)
+}
+function indexFn() {
+    var oIndex = id('index');
+    var oBtn = oIndex.getElementsByClassName('btn')[0];
+    var oInfo = oIndex.getElementsByClassName('info')[0];
+    bind(oBtn, 'touchend', endFn);
+    function endFn() {
+        if (scoreCheckedFn()) {
+            if (tagsCheckedFn()) {
+                indexOutFn();
+            } else {
+                 infoFn(oInfo, '请给景区添加标签');
+            }
+        } else {
+             infoFn(oInfo, '请给景区评分');
+        }
+    }
+    function scoreCheckedFn() {
+        var oScore = id('score');
+        var aInput = oScore.getElementsByTagName('input');
+        for ( var i = 0; i < aInput.length; i++) {
+            if (aInput[i].value == 0) {
+                return false;
+            }
+        }
+        return true;
+    };
+    function tagsCheckedFn() {
+        var oTags = id('tags');
+        var aInput = oTags.getElementsByTagName('input');
+        for ( var i = 0; i < aInput.length; i++) {
+            if (aInput[i].checked) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function indexOutFn() {
+    var oMask = id('mask');
+    var oIndex = id('index');
+    var oNews = id('news');
+    addClass(oMask,' pageShow');
+    addClass(oNews,' pageShow');
+    newsFn();
+    setTimeout(function() {
+        oMask.style.opacity = 1;
+        oIndex.style.filter = 'blur(10px)';
+        oIndex.style.WebkitFilter = 'blur(10px)';
+    },20);
+    setTimeout(function() {
+        oMask.style.opacity = 0;
+        oNews.style.transition = '0.5s';
+        oIndex.style.filter = 'blur(0px)';
+        oIndex.style.WebkitFilter = 'blur(0px)';
+        oNews.style.opacity = 1;
+    },3000);
+};
+
+function newsFn() {
+    var oNews = id('news');
+    var oInfo = oNews.getElementsByClassName('info')[0];
+    var aInput = oNews.getElementsByTagName('input');
+    aInput[0].onchange = function() {
+        if (this.files[0].type.split('/')[0] === 'video'){
+            newsOutFn();
+        } else {
+            infoFn(oInfo, '请上传视频');
+        }
+    }
+    aInput[1].onchange = function() {
+        if (this.files[0].type.split('/')[0] === 'image'){
+             newsOutFn();
+        } else {
+            infoFn(oInfo, '请上传图片');
+        }
+    }
+};
+function  newsOutFn() {
+    var oNews = id('news');
+    var oForm = id('form');
+    addClass(oForm,'pageShow');
+    oNews.style.cssText = '';
+    removeClass(oNews,'pageShow');
+    formIn();
+}
+
+function formIn() {
+    var oForm = id('form');
+    var oOver = id('over');
+    var aFormTags = id('form').getElementsByTagName('label');
+    var oBtn = oForm.getElementsByClassName('btn')[0];
+    var isOff = false;
+    for (var i = 0; i < aFormTags.length; i++) {
+        bind(aFormTags[i], 'touchend', function() {
+            isOff = true;
+            addClass(oBtn,'submit');
+        })
+    }
+    bind(oBtn, 'touchend', function() {
+        if (isOff) {
+            addClass(oOver, 'pageShow');
+            removeClass(oForm, 'pageShow');
+            over();
+        }
+    })
+}
+
+function over() {
+    var oOver = id('over');
+    var oBtn = oOver.getElementsByClassName('btn')[0];
+    bind(oBtn, 'touchend', function() {
+        removeClass(oOver,'pageShow');
+    })
 }
